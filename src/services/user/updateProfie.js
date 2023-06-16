d5683fc550ce54a3e42f01a613e97cc8d676d86b
seif0099 <seifbenhassen99@gmail.com>
2023-06-16 14:24:54 +0200

    profile

---
+++ src/services/user/updateProfie.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const baseURL = "http://127.0.0.1:8000/api";
+export const userUpdate = (
+  username,
+  lastname,
+  firstname,
+  password,
+  email,
+  address,
+  age,
+  phone,
+  image,
+  cv,
+  cover
+) => {
+  return new Promise((res, rej) => {
+    const formData = new FormData();
+    formData.append("nom", lastname);
+    formData.append("prenom", firstname);
+    formData.append("userName", username);
+    formData.append("image", image);
+    formData.append("adresse", address);
+    formData.append("age", age);
+    formData.append("telephone", phone);
+    formData.append("lettreMotivation", cover);
+    formData.append("cv", cv);
+    formData.append("email", email);
+    formData.append("password", password);
+    axios
+      .put(`${baseURL}/userupdate`, formData)
+      .then((response) => res(response.data))
+      .catch((err) => rej(err));
+  });
+};
